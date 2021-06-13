b4b3ecf9841a0c015abddbc4546dd83ba0f12ecc
jmoralesss <mail@jmorales.net>
2021-06-13 14:36:27 -0500

    created RWMutex example

--- sync-examples/rwmutex.go
+++ sync-examples/rwmutex.go
@@ -1,1 +1,64 @@
 package syncexamples
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"sync"
+	"text/tabwriter"
+	"time"
+)
+
+func RWMutexExample() {
+	producer := func(wg *sync.WaitGroup, locker sync.Locker) {
+		defer wg.Done()
+
+		for i := 5; i > 0; i-- {
+			locker.Lock()
+			locker.Unlock()
+			time.Sleep(1)
+		}
+	}
+
+	observer := func(wg *sync.WaitGroup, locker sync.Locker) {
+		defer wg.Done()
+		locker.Lock()
+		defer locker.Unlock()
+	}
+
+	test := func(count int, mutex, rwMutex sync.Locker) time.Duration {
+		var wg sync.WaitGroup
+
+		wg.Add(count + 1)
+
+		beginTestTime := time.Now()
+
+		go producer(&wg, mutex)
+
+		for i := count; i > 0; i-- {
+			go observer(&wg, rwMutex)
+		}
+
+		wg.Wait()
+
+		return time.Since(beginTestTime)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 0, 1, 2, ' ', 0)
+	defer tw.Flush()
+
+	var m sync.RWMutex
+	fmt.Fprintf(tw, "Readers\tRWMutext\tMutext\n")
+
+	for i := 0; i < 20; i++ {
+		count := int(math.Pow(2, float64(i)))
+
+		fmt.Fprintf(
+			tw,
+			"%d\t%v\t%v\n",
+			count,
+			test(count, &m, m.RLocker()),
+			test(count, &m, &m),
+		)
+	}
+}
